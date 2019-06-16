df795b0903f89a7246e76c02ca407e749bc4bf04
Itheima_YM <Itheima_YM@itcast.cn>
2019-06-17 00:04:24 +0800

    fitst commit test_git_prj

---
+++ test_git_prj/src/com/itheima/TestMain.java
@@ -0,0 +1,15 @@
+package com.itheima;
+
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("sssssssssssssss");
+        System.out.println("sssssssssssssss");
+        System.out.println("sssssssssssssss");
+        System.out.println("sssssssssssssss");
+    }
+
+
+    public void f(){
+
+    }
+}
